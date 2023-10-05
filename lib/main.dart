4067c1ae67a06a893757c9024e6721c18b71a698
AndreasH <Andreashavnen@gmail.com>
2023-10-06 00:39:50 +0200

    Added color scheme from seed "blueAccent"

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,31 @@ void main() {
     MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData().copyWith(
-            useMaterial3: true,
-            colorScheme: kColorScheme,
-            appBarTheme: AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer,
-              centerTitle: true,
-            )
-            //scaffoldBackgroundColor: Color.fromARGB(202, 4, 107, 155),
-            ),
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+            centerTitle: true,
+          ),
+          cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              )),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 25,
+                ),
+              ),
+        ),
         home: Expenses()),
   );
 }
