f468c135c15723dfffdb7ca5e0b294ecd31ae5db
AndreasH <Andreashavnen@gmail.com>
2023-10-05 18:54:14 +0200

    Added input and buttons in new_expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,17 +10,54 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _valueController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
-      child: Column(children: [
-        TextField(
-          maxLength: 50,
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(label: Text("Title")),
-        ),
-      ]),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            controller: _valueController,
+            maxLength: 50,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(label: Text("Value")),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_valueController.text);
+                },
+                child: const Text("Save Expense"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_valueController.text);
+                },
+                child: const Text("Cancel"),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
